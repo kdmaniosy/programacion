2c382c6447c4f0b5fe0efd5f314949c7168a89cb
kdmaniosy <kdmaniosy@ufpso.edu.co>
2024-03-01 11:31:08 -0500

    creación de clase interna

--- poo/src/main/java/com/mycompany/poo/Poo.java
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -1,16 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.poo;
 
-/**
- *
- * @author USUARIO
- */
 public class Poo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("Homero", "Inspector de seguridad", 33, "A la grande le puse cuca");
+        homero.greeting();
+        System.out.println(homero.age);
+        Character bart = new Character("Bartolomeo", "Vago", 10, "Ay caramba");
+        bart.greeting();
+    }
+    
+    static class Character {
+        String name;
+        String occupation;
+        int age;
+        String phrase;
+
+        public Character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+        
+        
+        
+        void greeting(){
+            System.out.println(this.phrase);
+        }
     }
 }
