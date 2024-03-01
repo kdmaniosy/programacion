c6aa832163c28dd4e50f36d126d41f43407c2ef5
kdmaniosy <kdmaniosy@ufpso.edu.co>
2024-03-01 10:42:17 -0500

    creación del proyecto

---
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
